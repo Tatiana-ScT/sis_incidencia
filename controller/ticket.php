e38e9c22020704d4970e6b5038b001d5a242a029
Tatiana-ScT <xxtai927xx@gmail.com>
2025-05-22 17:13:00 -0500

    second commit

---
+++ controller/ticket.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("../config/conection.php");
+    require_once("../models/Ticket.php");
+    $ticket = new Ticket();
+
+    switch($_GET["op"]){
+        case "insert":
+            $ticket = insert_ticket($_POST["id_usuario"], $_POST["ofi_id"], $_POST["tick_titulo"], $_POST["tick_descrip"]);    
+        break;
+    }
+?>
--- view/newTicket/newTicket.js
+++ view/newTicket/newTicket.js
@@ -1,9 +1,32 @@
+function init(){
+    $("#ticket_form").on("submit",function(e){
+        guardaryeditar(e);
+    })
+}
+
 $(document).ready(function() {
     $('#ticket_descrip').summernote({
         height: 200,
     });
 
     $.post("../../controller/oficina.php?op=combo",function(data,status){
-        console.log(data);
+        $('#ofi_id').html(data); 
     });
 });
+
+function guardaryeditar(e){
+    e.preventDefault();
+    var formData = new FormData($("#ticket_form")[0]);
+    $.ajax({
+        url:"../../controller/ticket.php?op=insert",
+        type:"POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        sucess: function(datos){
+            console.log(datos);
+        } 
+    })
+}
+
+init();
